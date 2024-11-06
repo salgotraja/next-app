7ecfa74c1c680e68ef2c2873073811333c6708f3
Jagdish Salgotra <jagdishsal@gmail.com>
2024-11-06 22:23:45 +0530

    Setup light and dark theme, shadcn, Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import Navbar from "@/components/navigation/navbar";
+import ThemeProvider from "@/context/Theme";
+
 import type { Metadata } from "next";
 
 import localFont from "next/font/local";
@@ -32,11 +35,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
