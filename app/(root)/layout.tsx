9928bfe0b1e13bd3ff2eec3e306e3074b447b7c4
Jagdish Salgotra <jagdishsal@gmail.com>
2024-11-09 17:41:13 +0530

    Search UI

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 
 import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar";
@@ -15,6 +15,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
+
         <RightSidebar />
       </div>
     </main>
