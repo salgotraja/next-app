f4cfd125579d9eedf52b5324e17922d6e62d768a
Jagdish Salgotra <jagdishsal@gmail.com>
2024-11-13 22:30:17 +0530

    question and answer model

---
+++ database/template.model.ts
@@ -0,0 +1,9 @@
+import { Schema, Types, model, models } from "mongoose";
+
+export interface IModel {}
+
+const ModelSchema = new Schema<IModel>({}, { timestamps: true });
+
+const Model = models?.Account || model<IModel>("Account", ModelSchema);
+
+export default Model;
